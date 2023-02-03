625f5f8847470433608d38b0fb1f7061fca125cc
pravinkumar-indianic <96461714+pravinkumar-indianic@users.noreply.github.com>
2023-02-03 17:46:38 +0530

    Update and rename CmsPagesServiceProvider.php to CMSPagesServiceProvider.php

--- src/CMSPagesServiceProvider.php
+++ src/CMSPagesServiceProvider.php
@@ -1,16 +1,14 @@
 <?php
-namespace Indianic\CmsPages;
+namespace Indianic\CMSPages;
 
-
+use Indianic\CMSPages\Nova\Resources\CMSPage;
+use Indianic\CMSPages\Policies\CMSPagePolicy;
 use Laravel\Nova\Nova;
-use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Events\ServingNova;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\ServiceProvider;
-use Indianic\CmsPages\Nova\Resources\CmsPages;
-use Indianic\CmsPages\Policies\CmsPolicy;
+use Illuminate\Support\Facades\Gate;
 
-class CmsPagesServiceProvider extends ServiceProvider
+class CMSPagesServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
@@ -18,34 +16,20 @@ class CmsPagesServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {   
+    {
         $this->setModulePermissions();
 
-        Gate::policy(\Indianic\CmsPages\Models\CmsPages::class, CmsPolicy::class);
+        Gate::policy(\Indianic\CMSPages\Models\CMSPage::class, CMSPagePolicy::class);
 
         Nova::serving(function (ServingNova $event) {
 
             Nova::resources([
-                CmsPages::class,
+                CMSPage::class,
             ]);
 
         });
 
-        if ($this->app->runningInConsole()) {
-
-            $this->loadMigrationsFrom(base_path('vendor/indianic/cms-pages/database/migrations'));
-            $path = 'vendor/indianic/cms-pages/database';
-            $migrationPath = $path."/migrations";
-            if (is_dir($migrationPath)) {
-                foreach (array_diff(scandir($migrationPath, SCANDIR_SORT_NONE), [".",".."]) as $migration) {
-                    Artisan::call('migrate', [
-                        '--path' => $migrationPath."/".$migration
-                    ]);
-                }
-            }
-        }
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
     }
 
     /**
@@ -53,46 +37,45 @@ class CmsPagesServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-        /**
+    /**
      * Set Cms Pages module permissions
      *
      * @return void
      */
-
-
-    private function setModulePermissions()
+    private function setModulePermissions(): void
     {
         $existingPermissions = config('nova-permissions.permissions');
 
         $existingPermissions['view cms-pages'] = [
             'display_name' => 'View cms pages',
             'description'  => 'Can view cms pages',
-            'group'        => 'Cms Pages'
+            'group'        => 'CMS Page'
         ];
 
         $existingPermissions['create cms-pages'] = [
             'display_name' => 'Create cms pages',
             'description'  => 'Can create cms pages',
-            'group'        => 'Cms Page'
+            'group'        => 'CMS Page'
         ];
 
         $existingPermissions['update cms-pages'] = [
             'display_name' => 'Update cms pages',
             'description'  => 'Can update cms pages',
-            'group'        => 'Cms Pages'
+            'group'        => 'CMS Page'
         ];
 
         $existingPermissions['delete cms-pages'] = [
             'display_name' => 'Delete cms pages',
             'description'  => 'Can delete cms pages',
-            'group'        => 'Cms Pages'
+            'group'        => 'CMS Page'
         ];
 
         \Config::set('nova-permissions.permissions', $existingPermissions);
     }
 }
+
